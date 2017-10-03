4e6ba2ff5c17ee258491f6b156e118c55e9b3ea0
mennokreukniet <menno.kreukniet@hotmail.com>
2017-10-03 09:25:02 +0200

    Homepage (not finished), tutorial (done), AdminLTE added.

--- application/views/users/registration.php
+++ application/views/users/registration.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">  
 <head>
-<link href="<?php echo base_url(); ?>/assets/css/style.css" rel='stylesheet' type='text/css' />
+<link href="<?php echo base_url(); ?>assets/css/style.css" rel='stylesheet' type='text/css' />
 </head>
 <body>
 <div class="container">
