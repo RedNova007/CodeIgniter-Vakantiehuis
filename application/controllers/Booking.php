4285a469dcb843526037335cad763a540e57a132
Kevin Cohen <99036164@mydavinci.nl>
2017-12-08 14:20:24 +0100

    booking 0.1 (notworkingyet)

--- application/controllers/Booking.php
+++ application/controllers/Booking.php
@@ -6,12 +6,56 @@ class Booking extends CI_Controller {
 	public function __construct()
     {
         parent::__construct();
+        $this->load->library('form_validation');
         $this->load->model('Booking_model');
     }
 
     public function index() 
     {
-        $this->load->view('booking/Booking_Form.php');    
+		if($this->session->userdata('isUserLoggedIn')){
 
-    }
+            
+            
+           
+            
+                $data = array();
+                $bookingData = array();
+                if($this->input->post('regisHomeSubmit')){
+                	$this->form_validation->set_rules('name', 'Name', 'required');
+
+                    $bookingData = array(
+                        'booking_name' => strip_tags($this->input->post('booking_name')),
+                        'booking_email' => strip_tags($this->input->post('booking_email')),
+                        'booking_guests' => strip_tags($this->input->post('booking_guests')),
+                        'arrival' => strip_tags($this->input->post('arrival')),
+                        'departure' => strip_tags($this->input->post('departure')),
+                        'booking_pickup' => strip_tags($this->input->post('booking_pickup')),
+                        'booking_requests' => strip_tags($this->input->post('booking_requests')),
+                        
+
+                    );
+
+                    if($this->form_validation->run() == true){
+                        $insert = $this->Booking_model->insert($bookingData);
+                        if($insert){
+                            redirect('booking/succes');
+                        }else{
+                            $data['error_msg'] = 'Some problems occured, please try again.';
+                        }
+                    }
+                }
+                $data['Booking_model'] = $bookingData;
+                //load the view
+                $this->load->view('booking/Booking_Form', $data);    
+
+            }
+
+            else{
+                 $this->load->view('Errors/index.html');
+            }
+        
+
+   
+        
+    } 
 }
