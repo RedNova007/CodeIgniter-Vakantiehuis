fa6d5d231f3d45d88f0c9891bfd3c888a95e627f
DESKTOP-8B3F4MK\MennoK <menno.kreukniet@hotmail.com>
2018-01-16 10:55:34 +0100

    Session fixes, na login redirect je nu naar homepage ipv account.

--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -4,13 +4,17 @@
      	logo
     </div>
     <ul class="navigation">
-	    	<li class="navigation2">
-	            <a href="<?php echo base_url(); ?>">Home</a></li>
-	        <li class="navigation2">
-	            <a href="<?php echo base_url(); ?>users/registration">Register</a></li> 
-	        <li class="navigation2">
-	            <a href="<?php echo base_url(); ?>users/login">Login</a></li>
-	        <!--<li class="navigation2">
-	        	<a href="<?php echo base_url(); ?>users/account">Account</a></li>-->
+	    <li class="navigation2">
+	        <a href="<?php echo base_url(); ?>">Home</a></li>
+	    <li class="navigation2" >
+	       	<a href="<?php if($this->session->userdata('isUserLoggedIn')){ echo base_url(); ?>users/account">Account</a></li>
+	    <li class="navigation2">
+	      	<a href="<?php echo base_url(); ?>users/logout">Logout</a></li>
+	    <?php }else{ ?>
+	    <li class="navigation2">
+	      	<a href="<?php echo base_url(); ?>users/registration">Register</a></li>
+	    <li class="navigation2">
+	        <a href="<?php echo base_url(); ?>users/login">Login</a></li>
+	    <?php } ?>
     </ul>
 </div>
