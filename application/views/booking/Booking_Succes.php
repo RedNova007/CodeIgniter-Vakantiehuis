70539586a2b82a7754c5c4e641ae43c9e862f4d6
Jazz Walter <jazz.walter@Liquit.com>
2018-01-16 12:07:58 +0100

    booking

---
+++ application/views/booking/Booking_Succes.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="<?php echo base_url(); ?>assets/css/loginstyle.css" rel='stylesheet' type='text/css' />
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet">
+	<title>Booking Succes</title>
+</head>
+<body>
+	<div>
+		
+	
+		<h1>Thank you for booking by Vakantiehuis.nl</h1>
+			
+			<h2>the owner will contact you</h2>
+
+			<a href="<?php echo base_url(); ?> "><button class="btn-primary">Return to homepage</button></a>
+
+	</div>
+</body>
+</html>
