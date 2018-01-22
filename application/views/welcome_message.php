cf544d5ed4de978bd6ac00565a8acb580c1718e9
DESKTOP-8B3F4MK\MennoK <menno.kreukniet@hotmail.com>
2018-01-22 22:18:44 +0100

    Merge fix (Lost changes in homepage re-added)

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -16,12 +16,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div class="image">
 		<div class="search">
 			<form  method="post" action="search/results"  method="POST"> 
-        		<input type="text" class ="home_inputs search_input" name="query" value="<?php echo html_escape($query = null); ?>" placeholder="Search..">
+                <input type="text" class ="home_inputs search_input" name="query" value="<?php echo html_escape($query = null); ?>" placeholder="Search..">
                 <input type="date" min="<?php echo date("Y-m-d"); ?>" class ="home_inputs" name="query2" value="<?php echo html_escape($query2 = null); ?>"  placeholder="From">
                 <input type="date" min="<?php echo date("Y-m-d"); ?>" class ="home_inputs" name="query3" value="<?php echo html_escape($query3 = null); ?>"  placeholder="Until">
-                <input type="number" class ="home_inputs" name="query4" value="<?php echo html_escape($query4 = null); ?>"  placeholder="Guests">
-                <input type="date" class ="home_inputs" name="query2" value="<?php echo html_escape($query2 = null); ?>"  placeholder="From">
-                <input type="date" class ="home_inputs" name="query3" value="<?php echo html_escape($query3 = null); ?>"  placeholder="Until">
                 <input type="number" class ="home_inputs" name="query4" value="<?php echo html_escape($query4 = null); ?>"  placeholder="Guests">
 				<input type="submit" name="go" value="Search">
 			</form>
