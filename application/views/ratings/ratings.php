37cc1cad4931834924dffab1dab5ab0d00050476
Jazz Walter <jazz.walter@Liquit.com>
2018-01-23 00:36:13 +0100

    Fixed the radio instead of input on the ratings form for stars

--- application/views/ratings/ratings.php
+++ application/views/ratings/ratings.php
@@ -3,9 +3,11 @@
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link href="<?php echo base_url(); ?>assets/css/loginstyle.css" rel='stylesheet' type='text/css' />
+    <link href="<?php echo base_url(); ?>assets/css/stars.css" rel='stylesheet' type='text/css' />
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script type="<?php echo base_url(); ?>application/js/stars.js"></script>
 
 </head>
 <body>
@@ -36,8 +38,39 @@
                         <?php $id = $_GET ['id']; ?>
                         <form action="" method="post">
                             <!--  <?php var_dump($vacationhouse); ?> -->
-                            <input type="text" class="form-control" onpaste="return false;" style="margin-bottom: 5px;" placeholder="Your review..." name="description" >
-                            <input type="number" class="form-control" onpaste="return false;" onkeydown="return event.which >= 8 && event.which <= 53" placeholder="Rate with stars 1-5" name="stars" >
+                            <input type="text" class="form-control " onpaste="return false;" style="margin-bottom: 5px; margin-top: 5px;" placeholder="Your review..." name="description" >
+                            <!-- <input type="number" class="form-control" onpaste="return false;" onkeydown="return event.which >= 8 && event.which <= 53" placeholder="Rate with stars 1-5" name="stars" > -->
+                            <p style="color: white;">Rate with stars 1-5</p>
+                            <div class="radio">
+                                            <label>
+                                            <input type="radio" name="stars" value="1" style="color: white; ">
+                                            1 
+                                            </label>
+                                        </div> 
+                                        <div class="radio">
+                                            <label>
+                                              <input type="radio" name="stars" value="2" style="color: white;">
+                                              2
+                                            </label>
+                                        </div>
+                                        <div class="radio">
+                                            <label>
+                                              <input type="radio" name="stars" value="3" style="color: white;">
+                                              3
+                                            </label>
+                                        </div>
+                                        <div class="radio">
+                                            <label>
+                                              <input type="radio" name="stars" value="4" style="color: white;">
+                                              4
+                                            </label>
+                                        </div>
+                                        <div class="radio">
+                                            <label>
+                                              <input type="radio" name="stars" value="5" style="color: white;">
+                                              5
+                                            </label>
+                                        </div>
                             <input type="hidden" name="id" value="<?php echo $id; ?>" >
                             <input type="submit" name="homeReviewSubmit" class="btn-primary" value="Submit" >
                         </form>
